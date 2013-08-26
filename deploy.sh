7667310bf0a00f50bf0f7d2c560a8d9cb15faf23
Jay Anslow <jay@anslow.me.uk>
2013-08-26 13:09:23 +0100

    Added CNAME (for GitHub Pages domains)
    Added CNAME file for mapping www.oxfordragball.co.uk to GitHub Pages site.
    Updated deploy.sh to copy CNAME file and added usage file

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,28 @@
 BIN_DIR="bin"
 PUBLISH_DIR="gh-pages"
+USAGE_MSG="Usage: ./deploy.sh commit_message"
 
-#Prepare Directories
+#Create Directories and clone GitHub Branch
 if [ ! -d "$PUBLISH_DIR" ]; then
 	mkdir $PUBLISH_DIR
 	git clone git@github.com:janslow/rag-ball-2013.git -b gh-pages $PUBLISH_DIR
 fi
 
+# Clear Publish dir
 cd $PUBLISH_DIR
 rm -R *
+
 cp -fR ../$BIN_DIR/ .
+cp -f ../CNAME .
 
-if [ -n "$1" ]; then
-	COMMIT_MSG=$1
+if [ ! -n "$1" ]; then
+	echo "Please supply a commit message"
+	echo "$USAGE_MSG"
+elif [ -n "$2" ]; then
+	echo "Unknown second argument"
+	echo "$USAGE_MSG"
 else
-	COMMIT_MSG="Automated Commit"
-	echo "Using default commit message"
+	git add .
+	git commit -a -m "$1"
+	git push origin gh-pages
 fi
-
-git add .
-git commit -a -m "$COMMIT_MSG"
-git push origin gh-pages
