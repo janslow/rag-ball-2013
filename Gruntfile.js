9eb657d215990f47c796140b76463b351fd14eb4
Jay Anslow <jay@anslow.me.uk>
2013-09-21 01:34:52 +0100

    Moved config for Jekyll, Sass and Watch plugins into separate files
    Added homepage

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,14 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      options: {
-        bundleExec: true,
-      },
-      scss: {
-        files: {
-          'site/css/window.css':'scss/window.scss',
-          'site/css/simple.css':'scss/simple.scss',
-          'site/css/construction.css':'scss/construction.scss',
-        },
-      },
-    },
-    jekyll: {
-      options: {
-        src: 'site/',
-        dest: 'bin/debug/',
-        bundleExec: true,
-      },
-      build: {
+    site: grunt.file.readJSON('grunt/site.json'),
 
-      },
-      serve: {
-        options: {
-          serve: true,
-          port: 8000,
-        },
-      },
-    },
-    watch: {
-      options: {
-        interrupt: true,
-      },
-      scss: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass'],
-      },
-      jekyll: {
-        files: 'site/**/*',
-        tasks: ['jekyll:build'],
-        options: {
-          livereload: true,
-        }
-      }
-    },
+    // Compile Jekyll and SCSS
+    sass: grunt.file.readJSON('grunt/sass.json'),
+    jekyll: grunt.file.readJSON('grunt/jekyll.json'),
+
+    // Watch for changes
+    watch: grunt.file.readJSON('grunt/watch.json'),
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
