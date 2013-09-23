6f17cf87868aec2b894484647c7a28fe63a136d3
Jay Anslow <jay@anslow.me.uk>
2013-09-23 23:53:12 +0100

    Added Compass plugin to SASS

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source 'http://rubygems.org'
 gem "jekyll", "~> 1.2.1"
 
 gem "sass", "~> 3.2.10"
+gem "compass", "~> 0.12.2"
+
+# Speeds up Compass Sprite compile times
+gem "oily_png", "~> 1.1.0"
---
+++ config.rb
@@ -0,0 +1,16 @@
+http_path = "/"
+http_stylesheets_path = "/css"
+http_images_path = "/img"
+http_generated_images_path = "/img/sprites"
+http_javascripts_path = "/js"
+
+project_path = "site"
+css_dir = "css"
+images_dir = "img"
+generated_images_dir = "img/sprites"
+javascripts_dir = "js"
+
+sass_path = "scss"
+sprite_load_path = ["sprites"];
+
+relative_assets = false
